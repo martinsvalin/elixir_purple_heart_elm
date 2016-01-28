8379a221a89e4d01a62cacfaa663c7bc9ce944f0
Martin Svalin <martin@lite.nu>
2016-01-28 20:21:08 +0100

    showing the seats

--- seat_saver/web/elm/SeatSaver.elm
+++ seat_saver/web/elm/SeatSaver.elm
@@ -1,7 +1,47 @@
 module SeatSaver where
 
 import Html exposing (..)
+import Html.Attributes exposing (class)
+-- import Graphics.Element
 
-main : Html
 main =
-  Html.text "Hello from Elm"
+  view init
+
+-- MODEL
+
+type alias Seat =
+  { seatNo : Int
+  , occupied : Bool
+  }
+
+type alias Model =
+  List Seat
+
+init : Model
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+
+
+-- VIEW
+
+view : Model -> Html
+view model =
+  ul [ class "seats" ] (List.map seatItem model)
+
+  -- Graphics.Element.show (Debug.log "foo" model)
+
+seatItem : Seat -> Html
+seatItem seat =
+  li [ class "seat available" ] [ text (toString seat.seatNo) ]
